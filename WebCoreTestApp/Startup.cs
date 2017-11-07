0d673eea928a044915ee1a01b5f9cae89ef6c08e
Vladimir Zemskov <vladimir.zemskov@unodc.org>
2017-11-07 11:40:36 +0100

    Added contoller

--- WebCoreTestApp/Startup.cs
+++ WebCoreTestApp/Startup.cs
@@ -15,6 +15,7 @@ namespace WebCoreTestApp
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddMvc();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -30,8 +31,14 @@ namespace WebCoreTestApp
             //    await context.Response.WriteAsync("Hello World!");
             //});
 
-            app.UseDefaultFiles();
+            //app.UseDefaultFiles();
             app.UseStaticFiles();
+            app.UseMvc(routes => 
+            {
+                routes.MapRoute("Default",
+                    "{controller}/{action}/{id?}",
+                    new { controller = "App", action = "Index"});
+            });
         }
     }
 }
