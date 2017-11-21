6754200cce923fd774e8b1066bdda1a0f081d31e
Vladimir Zemskov <vladimir.zemskov@unodc.org>
2017-11-21 11:14:47 +0100

    Added Angular support

---
+++ WebCoreTestApp/wwwroot/clientapp/out-tsc/wwwroot/ts/main.js
@@ -0,0 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var storecustomer_1 = require("./storecustomer");
+var shopper = new storecustomer_1.StoreCustomer("Test", "Tester");
+shopper.showName();
+//# sourceMappingURL=main.js.map
